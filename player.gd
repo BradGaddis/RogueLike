afc3a383d2f752aa8b6da5b159ebaf0315bcfde7
bradgaddis <bradleygad@gmail.com>
2023-09-23 20:40:33 -0400

    add basic level bg and add player movement

---
+++ player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity = Vector2.ZERO	
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -1 * speed
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -1 * speed
+		
+	move_and_slide()
